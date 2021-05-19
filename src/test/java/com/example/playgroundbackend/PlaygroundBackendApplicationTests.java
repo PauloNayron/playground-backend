aa362d7b66f7674f0919afe55fb400d668e9366c
PauloNayron <paulonayron.s@hotmail.com>
2021-05-19 09:30:27 -0300

    feat: Criação de Projeto Básico

---
+++ src/test/java/com/example/playgroundbackend/PlaygroundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playgroundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
