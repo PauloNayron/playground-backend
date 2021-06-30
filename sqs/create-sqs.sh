0e910a928cf4861219ac781c5dd4c9bc754fc84f
PauloNayron <paulonayron.s@hotmail.com>
2021-06-29 22:59:11 -0300

    fix: ajuste de pom

---
+++ sqs/create-sqs.sh
@@ -0,0 +1,10 @@
+#aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name playground-sqs
+
+# lista as filas
+#aws --endpoint-url=http://localhost:4576 sqs list-queues
+
+# envia mensagem para a fila
+aws --endpoint-url=http://localhost:4576 sqs send-message --queue-url http://localhost:4576/000000000000/playground-sqs --message-body 'Hello World!'
+
+# consume a fila
+#aws --endpoint-url=http://localhost:4576 sqs receive-message --queue-url http://localhost:4566/000000000000/playground-sqs
